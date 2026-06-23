aed63d528ffadac047c5d9ce44d82cd99b85b145
Lucas Arcoverde <lucasmarcoverde@gmail.com>
2026-06-23 10:49:09 -0300

    make get cwd and print it.

--- ash.c
+++ ash.c
@@ -2,12 +2,31 @@
 #include <unistd.h>
 
 char login[256]; // "username"
+char cwd[1024]; // Current Working Directory
 
 // get user login
 void get_login()
 {
     login[255] = '\0';
     getlogin_r(login, sizeof(login));
+}
+
+void get_cwd()
+{
+    // try to get cwd. if it fails, exit function.
+    if (getcwd(cwd, sizeof(cwd)) == NULL)
+    {
+        printf("Could not get current working directory!\n");
+        return;
+    }
+}
+
+void print_login_and_cwd()
+{
+    get_login();
+    get_cwd();
+
+    printf("%s at %s: \n", login, cwd);
 }
 
 void print_banner()
@@ -24,8 +43,8 @@ void print_banner()
 
 int main(void)
 {
-    get_login();
     print_banner();
+    print_login_and_cwd();
     
     return 0;
 }
