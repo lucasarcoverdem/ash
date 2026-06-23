d35436b91e55148c493f35a8d0ff593091e36c52
Lucas Arcoverde <lucasmarcoverde@gmail.com>
2026-06-23 10:28:07 -0300

    make print a welcome banner.

--- ash.c
+++ ash.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+void print_banner()
+{
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+    printf("|                    -------                     |\n");
+    printf("|                    | ASH |                     |\n");
+    printf("|                    -------                     |\n");
+    printf("|                    a shell                     |\n");
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+
+    printf("\n                 Welcome to ASH!\n\n");
+}
+
 int main(void)
 {
+    print_banner();
     return 0;
 }
 
